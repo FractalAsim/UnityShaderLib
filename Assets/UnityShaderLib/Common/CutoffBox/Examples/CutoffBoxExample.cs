206047227ea2ab1d31f51b9efc552cc04afa35ce
Khiew Jian Bin <khiewbin@gmail.com>
2025-07-14 04:30:16 +0800

    add cutoffbox, and cutoffbox shadergraph

---
+++ Assets/UnityShaderLib/Common/CutoffBox/Examples/CutoffBoxExample.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class CutoffBoxExample : MonoBehaviour
+{
+    readonly int enablePropertyID = Shader.PropertyToID("_Enable");
+    readonly int minXPropertyID = Shader.PropertyToID("_MinX");
+    readonly int maxXPropertyID = Shader.PropertyToID("_MaxX");
+    readonly int minYPropertyID = Shader.PropertyToID("_MinY");
+    readonly int maxYPropertyID = Shader.PropertyToID("_MaxY");
+    readonly int minZPropertyID = Shader.PropertyToID("_MinZ");
+    readonly int maxZPropertyID = Shader.PropertyToID("_MaxZ");
+
+    public bool EnableCutoff = true;
+
+    [SerializeField] Transform Box;
+    [SerializeField] Transform CutoffBox;
+
+    [SerializeField] Transform BoxSG;
+    [SerializeField] Transform CutoffBoxSG;
+
+    Vector3 CutoffBox_startPos = new();
+    Vector3 CutoffBoxSG_startPos = new();
+
+    void Start()
+    {
+        if (CutoffBox == null) return;
+
+        CutoffBox_startPos = CutoffBox.localPosition;
+
+        if (CutoffBoxSG == null) return;
+
+        CutoffBoxSG_startPos = CutoffBoxSG.localPosition;
+    }
+
+    void Update()
+    {
+        if (Box == null || CutoffBox == null) return;
+
+        UpdateBox(Box, CutoffBox, CutoffBox_startPos);
+
+        if (BoxSG == null || CutoffBoxSG == null) return;
+
+        UpdateBox(BoxSG, CutoffBoxSG, CutoffBoxSG_startPos);
+
+    }
+    void UpdateBox(Transform Box, Transform CutoffBox, Vector3 startPos)
+    {
+        CutoffBox.localPosition = startPos + new Vector3(Mathf.Sin(Time.time * 2) * 2 - 1, 0, 0);
+
+        var pos = CutoffBox.position;
+        var scale = CutoffBox.lossyScale;
+
+        var min = pos - scale / 2;
+        var max = pos + scale / 2;
+        var minX = min.x;
+        var maxX = max.x;
+        var minY = min.y;
+        var maxY = max.y;
+        var minZ = min.z;
+        var maxZ = max.z;
+
+        var sharedMaterial = Box.GetComponent<Renderer>().sharedMaterial;
+        sharedMaterial.SetFloat(enablePropertyID, EnableCutoff ? 1 : 0);
+        sharedMaterial.SetFloat(minXPropertyID, minX);
+        sharedMaterial.SetFloat(maxXPropertyID, maxX);
+        sharedMaterial.SetFloat(minYPropertyID, minY);
+        sharedMaterial.SetFloat(maxYPropertyID, maxY);
+        sharedMaterial.SetFloat(minZPropertyID, minZ);
+        sharedMaterial.SetFloat(maxZPropertyID, maxZ);
+    }
+}
---
+++ Assets/UnityShaderLib/Common/CutoffBox/Shader/CutoffBox.shader
@@ -0,0 +1,78 @@
+// Common Shader that cuts off the fragment out side of a box range
+
+Shader "Common/CutoffBox"
+{
+    Properties
+    {
+        _Color ("Color", Color) = (1,1,1,1)
+
+        _Enable("Enable", float) = 0
+        _MinX("Min X", float) = 0
+        _MaxX("Max X", float) = 1
+        _MinY("Min Y", float) = 0
+        _MaxY("Max Y", float) = 1
+        _MinZ("Min Z", float) = 0
+        _MaxZ("Max Z", float) = 1
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            Cull Off 
+
+            CGPROGRAM
+
+            #pragma vertex vert // Use "vert" function for Vertex Shader
+            #pragma fragment frag // Use "frag" function for Fragment Shader
+
+            // Input to Vertex Shader
+            struct appdata
+            {
+                float4 vertex : POSITION;
+            };
+
+            // Input to Fragment Shader
+            struct v2f
+            {
+                float4 pos : SV_POSITION;
+                float3 worldPos : TEXCOORD0;
+            };
+
+            float4 _Color;
+
+            // For Cutoff
+            fixed _Enable;
+            fixed _MinX;
+            fixed _MaxX;
+            fixed _MinY;
+            fixed _MaxY;
+            fixed _MinZ;
+            fixed _MaxZ;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.pos = UnityObjectToClipPos(v.vertex);
+                o.worldPos = mul(unity_ObjectToWorld, v.vertex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                float x = (i.worldPos.x - _MinX) * (_MaxX - i.worldPos.x);
+                float y = (i.worldPos.y - _MinY) * (_MaxY - i.worldPos.y);
+                float z = (i.worldPos.z - _MinZ) * (_MaxZ - i.worldPos.z);
+                float xyz = min(min(x, y), z);
+
+                clip(xyz * _Enable);
+
+                return _Color;
+            }
+
+            ENDCG
+        }
+    }
+}
